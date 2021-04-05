ec02816b16b7f5e794fad4d1a1235a0b86093677
felipe <felipephplow@gmail.com>
2021-04-05 10:59:14 -0300

    Alterado
    10%

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -56,7 +56,7 @@
 
                   </div>
                   <div class="col-md-4 col-10 mt-2 mb-4">
-                        <button class="btn btn-env">CADASTRAR E GANHAR 100%</button>
+                        <button class="btn btn-env">CADASTRAR E GANHAR 10%</button>
                   </div>
             </div>
       </section>
