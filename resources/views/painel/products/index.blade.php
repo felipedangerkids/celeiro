5c7a27515937b617f905dc073ce5e38ef4d1c759
felipe <felipephplow@gmail.com>
2021-04-13 12:06:51 -0300

    Modal Centralizado

--- resources/views/painel/products/index.blade.php
+++ resources/views/painel/products/index.blade.php
@@ -50,7 +50,7 @@
 {{-- modal --}}
 <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1"
       aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                   <div class="modal-header">
                         <h5 class="modal-title" id="staticBackdropLabel">Adicionar Produto</h5>
