6cbe4d6b625d819d028cc049dc2d7da21468ff9b
felipe cruz <felipephplow@gmail.com>
2021-07-05 00:06:58 -0300

    a

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,10 @@
         <div class="text-center my-5">
             <img src="{{ url('assets/img/logo.svg') }}" alt="">
         </div>
-        <div style="position: absolute; top: 50%; " class="text-center">
+        <div style="  position:absolute;
+        top:60%;
+        left:50%;
+        transform:translate(-50%,-50%) " class="text-center">
             <h1>Atualizando...aguarde voltaremos logo!!!</h1>
         </div>
     </div>
