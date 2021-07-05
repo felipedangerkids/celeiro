f7cf0ecee396c0f150645cf9cb1b8d34050828bb
felipe cruz <felipephplow@gmail.com>
2021-07-05 00:04:01 -0300

    Att

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+@section('content')
+    <div class="container">
+        <div class="text-center my-5">
+            <img src="{{ url('assets/img/logo.svg') }}" alt="">
+        </div>
+        <div style="position: absolute; top: 50%; " class="text-center">
+            <h1>Atualizando...aguarde voltaremos logo!!!</h1>
+        </div>
+    </div>
+@endsection
