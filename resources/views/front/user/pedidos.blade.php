bf0a99dcfa3ec8d9c13ed7c98b133b8f5fa64786
felipe cruz <felipephplow@gmail.com>
2021-07-26 16:38:30 -0300

    das

--- resources/views/front/user/pedidos.blade.php
+++ resources/views/front/user/pedidos.blade.php
@@ -4,44 +4,38 @@
         <div class="text-center pt-4">
             <h1>SEUS <br> PEDIDOS</h1>
         </div>
-
-        <table class="table text-white">
-            <thead>
-                <tr>
-                    <th scope="col">Numero</th>
-                    <th scope="col">Pagamento</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Ver</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($pedidos as $pedido)
-                    <tr>
-                        <th scope="row">{{ $pedido->id }}</th>
-                        <td>{{ $pedido->pagamento }}</td>
-                        <td>
-                            @if ($pedido->status == 0)
-                                Aguardando
-                            @elseif ($pedido->status == 1)
-                                Em preparação
-                            @elseif ($pedido->status == 2)
-                                Saiu para entrega
-                            @elseif ($pedido->status == 3)
-                                Entregue
-                            @elseif ($pedido->status == 4)
-                                Cancelado
-                            @endif
-                        </td>
-                        <td>
-                            <div>
-                                <button type="button" class="btn btn-warning">Detalhes</button>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-
-            </tbody>
-        </table>
-
+        <div class="text-center pt-4">
+            <button class="btn btn-continuar">Voltar</button>
+        </div>
     </div>
+    @foreach ($pedidos as $pedido)
+
+        <div class="pedido my-4">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 text-center ">
+                            <p>PEDIDO</p>
+                        </div>
+                        <div class="col-12 text-center font">
+                            <p><strong>PEDIDO</strong> #00{{ $pedido->id }}</p>
+                        </div>
+                        <div class="col-12 text-center font">
+                            <p><Strong>PAGAMENTO</strong> {{ $pedido->pagamento }}</p>
+                        </div>
+                        <div class="col-12 text-center font">
+                            <p><strong>STATUS:</strong> {{ $pedido->status }}</p>
+                        </div>
+
+                    </div>
+                    <div class="text-center">
+                        <button class="btn btn-dark">DETALHES</button>
+                    </div>
+                </div>
+        </div>
+
+    @endforeach
+
+
+
+
 @endsection
