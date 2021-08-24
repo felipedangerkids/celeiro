ead267dd09559c70c1a77aa87165cec5e5dc2646
felipe cruz <felipephplow@gmail.com>
2021-08-24 15:50:49 -0300

    l

--- resources/views/front/home/inicio.blade.php
+++ resources/views/front/home/inicio.blade.php
@@ -1,84 +1,99 @@
 @extends('layouts.main')
 @section('content')
-    <div class="container">
-        {{-- @if (auth()->check())
-        <div class="profile mt-5">
-            <div class="d-flex">
-                <div>
-                    <a href="">
-                        <img src="{{ url('assets/img/Group 1.png') }}" alt="">
-                    </a>
-                </div>
-                <div class="spans">
-                    <div class="receber">
-                        <span>Receber agora em:</span> <br>
-                    </div>
-                    <div class="endereco">
-                        <a href="/perfil">
-                            <span>Avenida do Batel, 600-Batel, Curitiba-PR</span>
+<div class="container">
+    @if (auth()->guard('cliente')->check())
+        <div class="container">
+            <div class="profile mt-5">
+                <div class="d-flex">
+                    <div>
+                        <a href="">
+                            <img src="{{ url('assets/img/Group 1.png') }}" alt="">
                         </a>
                     </div>
+                    <div class="spans">
+                        <div class="receber">
+                            <span>Receber agora em:</span> <br>
+                        </div>
+                        <div class="endereco">
+                            <a href="/perfil">
+                                <span>Avenida do Batel, 600-Batel, Curitiba-PR</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        @endif --}}
-        <div class="sub-logo mt-4">
-            <img src="{{ url('assets/img/Camada x0020 3.png') }}" alt="">
-        </div>
-
-        <div class="submit-line mt-3 text-center">
-            <form action="{{ route('search') }}" method="POST">
-                @csrf
-                <input type="texte" name="search" placeholder="  Busque o que você ama..." />
-                <button class="submit-lente" type="submit">
-                    <i class="fa fa-search"></i>
-                </button>
-            </form>
-        </div>
-        <div class="icones-gerais mt-4">
-            <div class="d-flex text-center">
-                <div class="beer">
-                    <a href="{{ route('shop.cervejas') }}">
-                        <img src="{{ url('assets/img/beer 1.png') }}" alt="merg">
-                    </a>
-                </div>
-                <div class="beer">
-                    <a href="{{ route('shop.kits') }}">
-                        <img src="{{ url('assets/img/beer-box 1.png') }}" alt="">
-                    </a>
-                </div>
-                <div class="beer">
-                    <a href="{{ route('shop.embutidos') }}">
-                        <img src="{{ url('assets/img/sausage 1.png') }}" alt="">
-                    </a>
+    @else
+        <div class="container">
+            <div class="profile mt-5">
+                <div class="d-flex">
+                    <div class="spans">
+                        <div class="endereco text-center">
+                            <a href="/perfil">
+                                <span>Faça login para ver seu endereço.</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="mt-5 text-center">
-            <h1>o que você <br> quer beber <br> hoje?</h1>
-            <img src="{{ url('assets/img/arrow-left 1.png') }}" alt="">
-        </div>
+    @endif
+    <div class="sub-logo mt-4">
+        <img src="{{ url('assets/img/Camada x0020 3.png') }}" alt="">
+    </div>
 
-        <div class="destaque-produtos mt-5">
-            <div class="text-center">
-                <div class="row">
-                    @foreach ($produtos as $produto)
-                        @if ($produto->spotlight == 1)
-                            <div style="margin-bottom: 9rem !important;" class="product col-5 mt-3 mb-5">
-                                <a href="{{ route('shop.single', $produto->id) }}">
-                                    <img style="object-fit: cover;" src="{{ url('storage/produtos/' . $produto->image) }}"
-                                        alt="">
-                                </a>
-                                <div class="caption">
-                                    <span>{{ $produto->name }}</span>
-                                </div>
+    <div class="submit-line mt-3 text-center">
+        <form action="{{ route('search') }}" method="POST">
+            @csrf
+            <input type="texte" name="search" placeholder="  Busque o que você ama..." />
+            <button class="submit-lente" type="submit">
+                <i class="fa fa-search"></i>
+            </button>
+        </form>
+    </div>
+    <div class="icones-gerais mt-4">
+        <div class="d-flex text-center">
+            <div class="beer">
+                <a href="{{ route('shop.cervejas') }}">
+                    <img src="{{ url('assets/img/beer 1.png') }}" alt="merg">
+                </a>
+            </div>
+            <div class="beer">
+                <a href="{{ route('shop.kits') }}">
+                    <img src="{{ url('assets/img/beer-box 1.png') }}" alt="">
+                </a>
+            </div>
+            <div class="beer">
+                <a href="{{ route('shop.embutidos') }}">
+                    <img src="{{ url('assets/img/sausage 1.png') }}" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="mt-5 text-center">
+        <h1>o que você <br> quer beber <br> hoje?</h1>
+        <img src="{{ url('assets/img/arrow-left 1.png') }}" alt="">
+    </div>
+
+    <div class="destaque-produtos mt-5">
+        <div class="text-center">
+            <div class="row">
+                @foreach ($produtos as $produto)
+                    @if ($produto->spotlight == 1)
+                        <div style="margin-bottom: 9rem !important;" class="product col-5 mt-3 mb-5">
+                            <a href="{{ route('shop.single', $produto->id) }}">
+                                <img style="object-fit: cover;" src="{{ url('storage/produtos/' . $produto->image) }}"
+                                    alt="">
+                            </a>
+                            <div class="caption">
+                                <span>{{ $produto->name }}</span>
                             </div>
-                        @endif
-                    @endforeach
-                </div>
+                        </div>
+                    @endif
+                @endforeach
             </div>
         </div>
-        <div class="fundo">
-        </div>
+    </div>
+    <div class="fundo">
+    </div>
     </div>
 @endsection
