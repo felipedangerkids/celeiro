5a97e2dbe35607b70864c17f2491bc9435b05ecb
Eller Jones Silveira <ellernetpar@gmail.com>
2021-10-07 10:12:59 -0300

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,8 @@
                     </div>
 
                     <div class="col-6 text-center my-3"><a class="btn btn-c-orange px-3 py-2" href="{{route('shop')}}">COMPRAR ONLINE</a></div>
-                    <div class="col-6 text-center my-3"><a class="btn btn-c-orange px-3 py-2" href="{{route('cashback')}}">ESTOU NO LOCAL</a></div>
-                    <div class="col-6 text-center my-3"><a class="btn btn-c-orange px-3 py-2" href="#">SEU CASHBACK</a></div>
+                    <div class="col-6 text-center my-3"><a class="btn btn-c-orange px-3 py-2" href="#">ESTOU NO LOCAL</a></div>
+                    <div class="col-6 text-center my-3"><a class="btn btn-c-orange px-3 py-2" href="{{route('cashback')}}">SEU CASHBACK</a></div>
                     <div class="col-6 text-center my-3"><a class="btn btn-c-orange px-3 py-2" href="{{route('perfil')}}">SEUS DADOS</a></div>
 
                     <div class="col-12 text-center my-3">
